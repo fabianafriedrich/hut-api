baefcd4523e6234dc099475aceaa627753530c1e
faiana <biafriedrich06@gmail.com>
2020-05-18 18:07:08 +0100

    changing satus type in DB.

--- src/main/java/com/cct/hut/api/enums/Status.java
+++ src/main/java/com/cct/hut/api/enums/Status.java
@@ -1,20 +1,6 @@
 package com.cct.hut.api.enums;
 
 public enum Status {
-    ANSWERED(1),
-    UNANSWERED(2);
-
-    private int status;
-
-    Status(int status) {
-        this.status = status;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
+    ANSWERED,
+    UNANSWERED;
 }
--- src/main/java/com/cct/hut/api/model/Post.java
+++ src/main/java/com/cct/hut/api/model/Post.java
@@ -26,6 +26,7 @@ public class Post implements Serializable{
 
     private String description;
 
+    @Enumerated(EnumType.STRING)
     private Status status;
 
     private Integer likes;
